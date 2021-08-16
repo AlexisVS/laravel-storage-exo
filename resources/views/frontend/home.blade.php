3f268b7f046458184d4c24eeb3ac75464a90b490
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-08-16 15:48:59 +0200

    finit

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,13 +1,27 @@
 @extends("frontend.template.index")
 @section('content')
     <div class="container-fluid">
+        <h1>Fichier Image</h1>
         <div class="row row-cols-3">
             @foreach ($fichiers as $fichier)
-                <div class="col">
-                    <img class="w-100 h-100" src="{{ asset('storage/img/' . $fichier->fileName) }}" alt="">
-                </div>
+                @if (Str::contains($fichier->fileName, ['.png', '.jpg', '.jpeg', 'webp']))
+                    <div class="col">
+                        <img class="w-100 h-100" src="{{ asset('storage/img/' . $fichier->fileName) }}" alt="">
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
-
+    <div class="container-fluid">
+        <h1>Fichier texte</h1>
+        <div class="row row-cols-3">
+            @foreach ($fichiers as $fichier)
+                @if (Str::contains($fichier->fileName, ['.txt', '.md']))
+                    <div class="col">
+                        <p>{{ $fichier->fileName }}</p>
+                    </div>
+                @endif
+            @endforeach
+        </div>
+    </div>
 @endsection
